b4b1a11270b267890b0876d57dbb5134942f11ba
majd19-meet <majd19@meet.mit.edu>
2017-07-30 10:55:15 +0200

    Stopping here

--- fun1.py
+++ fun1.py
@@ -5,11 +5,13 @@
 #print (c)
 
 
-def add_numbers () :
+def add_numbers (start, end) :
     c = 0
-    for number in range (1, 100 + 1):
-        print (number)
+    for number in range (start, end + 1):
+        #print (number)
         c = c + number
     return (c)
-answer = add_numbers ()
+
+
+answer = add_numbers (333, 777)
 print (answer)
---
+++ fun2.py
@@ -0,0 +1,8 @@
+#print ("*****")
+#print ("*")
+#print ("*")
+#print ("*")
+#print ("*")
+
+def draw_1d(n,char):
+    print (char*n)
